377efb8cb6dd170367d74edcc8599d9565f7a244
wsl3 <2350622075@qq.com>
2019-03-12 21:05:05 +0800

    增加表达式求值, 增加异常处理

--- caculate/caculate_interpreter.py
+++ caculate/caculate_interpreter.py
@@ -1,13 +1,14 @@
 """
     一个计算器的 Small interpreters
 """
-
+from functools import reduce
 
 # 基本的表达式 like 'add(1,2)'
 class Expr(object):
     def __init__(self, operator, num_list):
         self.operator = operator
         self.num_list = num_list
+        self._result = None
 
     def __repr__(self):
         return "Expr({}, {})".format(repr(self.operator), repr(self.num_list))
@@ -15,6 +16,30 @@ class Expr(object):
     def __str__(self):
         num_str = ", ".join(map(str, self.num_list))
         return "{}({})".format(str(self.operator), num_str)
+
+    # return the result of a Expr
+    @property
+    def result(self):
+        self.num_list = [self.generate_result(sym) for sym in self.num_list]
+        if self.operator in ("add", "+"):
+            return reduce(lambda x,y: x+y, self.num_list)
+        elif self.operator in ("sub", "-"):
+            return reduce(lambda x,y: x-y, self.num_list)
+        elif self.operator in ("mul", "*"):
+            return reduce(lambda x,y: x*y, self.num_list)
+        elif self.operator in ("div", "/"):
+            try:
+                return reduce(lambda x,y: x/y, self.num_list)
+            except ZeroDivisionError:
+                print("Error:除数不能为0!\n")
+                exit(0)
+
+    def generate_result(self, sym):
+        if(isinstance(sym, Expr)):
+            return sym.result
+        else:
+            return sym
+
 
 
 class Interpreter(object):
@@ -24,6 +49,12 @@ class Interpreter(object):
         self.tokens = []
 
     def parse(self):
+        """
+        parse函数中 first_token假定有两种情况
+        1. 为数字 则返回 数字本身,然后去掉数字后面的字符, 可能是 , ), 这样下一个first_token 可能为数字或者 add 之类的操作符
+        2. 为add之类的操作符,则返回 一个Expr 对象
+        :return: 数字或者Expr对象
+        """
         first_token = self.next_token()
         token = self.eat(first_token)
         if first_token == token:
@@ -67,6 +98,7 @@ class Interpreter(object):
 
 
 
-i = Interpreter("add(mul(1,2), mul(2,sub(3,4)))")
+i = Interpreter("add(1.5, sub(2,div(1,1)), mul(2,3))")
 i.tokenize()
-print(i.parse())
+expr = i.parse()
+print(expr.result)
